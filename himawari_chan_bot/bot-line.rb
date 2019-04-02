5e41664b96dd922961dab18e9620b3b29c1b0ac2
fieliapm <fieliapm@gmail.com>
2019-04-02 19:18:10 +0800

    add GPL3 terms

--- himawari_chan_bot/bot-line.rb
+++ himawari_chan_bot/bot-line.rb
@@ -1,6 +1,27 @@
 #!/usr/bin/env ruby
 # -*- coding: utf-8 -*-
 
+################################################################################
+#
+# himawari_chan_bot - a simple chat bot example
+#                     for line, telegram, slack, discord
+# Copyright (C) 2019-present Himawari Tachibana <fieliapm@gmail.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+################################################################################
+
 require 'json'
 require 'sinatra/base'
 require 'line/bot'
